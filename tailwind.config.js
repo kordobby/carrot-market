653be4a387dbbfbf80f5eeb9d77598f011887312
kordobby <poordobby@naver.com>
2022-08-21 16:07:50 +0900

    tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{tsx,ts,jsx,js}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
